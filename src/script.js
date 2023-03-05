0c1e7863492c090afd7127f5fc4dbd11cc860c27
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-03-05 00:04:23 -0300

    Add getCoupleSquares function which manage the state of the game and refactor getRandomColors

--- src/script.js
+++ src/script.js
@@ -1,12 +1,13 @@
 const squaresContainer = document.querySelector(".container");
 const squares = squaresContainer.querySelectorAll("div");
 const colors = ["red", "yellow", "orange", "blue", "pink", "green"];
+let coupleSquares = [];
 
 function getRandomColors() {
-  let copyArrayColors = colors.slice(0);
-  copyArrayColors.sort(() => Math.random() - 0.5);
+  let copyArrayColors = colors.concat(colors);
+  let shuffleArray = copyArrayColors.sort(() => Math.random() - 0.5);
 
-  return copyArrayColors.concat(copyArrayColors);
+  return shuffleArray;
 }
 
 function paintSquares() {
@@ -15,15 +16,58 @@ function paintSquares() {
   for (let i = 0; i < randomColors.length; i++) {
     squares[i].classList.add(randomColors[i]);
   }
+}
 
+function coverSquares() {
   squares.forEach(function (square) {
     square.classList.add("white");
+  });
+}
 
+function resetSquares() {
+  setTimeout(function () {
+    coupleSquares[0].classList.add("white");
+    coupleSquares[1].classList.add("white");
+  }, 400);
+}
+
+function deleteSquares() {
+  setTimeout(function () {
+    coupleSquares[0].style.pointerEvents = "none";
+    coupleSquares[1].style.pointerEvents = "none";
+  }, 400);
+}
+
+function getCoupleSquares(square) {
+  coupleSquares.push(square);
+
+  if (coupleSquares.length === 2) {
+    if (coupleSquares[0].className !== coupleSquares[1].className) {
+      resetSquares();
+    } else {
+      deleteSquares();
+    }
+  }
+
+  if (coupleSquares.length > 2) {
+    coupleSquares = [];
+    coupleSquares.push(square);
+  } 
+}
+
+function checkUserInput() {
+  squares.forEach(function (square) {
     square.onclick = function () {
+
       square.classList.remove("white");
+
+      getCoupleSquares(square);
     };
   });
 }
 
 paintSquares();
 
+coverSquares();
+
+checkUserInput();
